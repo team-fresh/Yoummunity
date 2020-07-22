40c478c0f8e07bc023624171697e7d10680d2e24
Soeun <iamsoeunlee@daum.net>
2020-07-22 16:38:01 +0900

    :arrow_up: maxResults = 20(default) -> 100

--- app/src/main/java/com/yoummunity/YouTubeAPI.kt
+++ app/src/main/java/com/yoummunity/YouTubeAPI.kt
@@ -8,9 +8,11 @@ import retrofit2.http.Query
 interface YouTubeAPI {
     @GET("v3/commentThreads")
     fun query(
+        // For more info, see [https://developers.google.com/youtube/v3/docs/commentThreads/list]   // API key
         @Query("key") key: String = "AIzaSyCEk4fukdGonbEO5Xwl98vY4M0W_av37w8",
         @Query("part") part: String = "snippet",
-        @Query("order") order: String = "relevance",
+        @Query("maxResults") maxResults: Int = 100,             // specifies the maximum number of items that should be returned in the result set.
+        @Query("order") order: String = "relevance",            // specifies the order in which the API response should list comment threads.
         @Query("videoId") videoId: String,
         @Query("pageToken") pageToken: String?
     ): Call<Data>
