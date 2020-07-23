bd1364fd3b85df74b917916133544fb84f9a5362
Soeun <iamsoeunlee@daum.net>
2020-07-23 17:09:15 +0900

    :art: prepared a place for the author's profile image

--- app/src/main/java/com/yoummunity/CommentsActivity.kt
+++ app/src/main/java/com/yoummunity/CommentsActivity.kt
@@ -11,6 +11,7 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.text.HtmlCompat
 import kotlinx.android.synthetic.main.activity_comments.*
 import kotlinx.android.synthetic.main.list_item_comment.*
@@ -18,15 +19,15 @@ import kotlinx.android.synthetic.main.list_item_comment.*
 class CommentsActivity : Activity() {
     private var text: String = ""
     private var textList = mutableListOf<CharSequence>()
-    private var arrayAdapter: ArrayAdapter<CharSequence>? = null
     private var listCommentsView: ListView? = null
+    private var adapter: CommentListAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_comments)
 
         listCommentsView = list_comments
-        arrayAdapter = ArrayAdapter<CharSequence>(this, R.layout.list_item_comment, textList)
+        adapter = CommentListAdapter()
 
         window.attributes.gravity =
             Gravity.BOTTOM + Gravity.RIGHT  // set position of dialog on window
@@ -40,14 +41,19 @@ class CommentsActivity : Activity() {
 //        var textView = textview_comment
 //        var data = intent.getStringExtra("data")
         for (i in GlobalClass.comments.indices) {
-            text =
-                "<b>" + GlobalClass.authors[i] + "</b><br>" + GlobalClass.comments[i]
+//            text =
+//                "<b>" + GlobalClass.authors[i] + "</b><br>" + GlobalClass.comments[i]
             // text: String in HTML
-            textList.add(HtmlCompat.fromHtml(text, HtmlCompat.FROM_HTML_MODE_LEGACY))
+//            textList.add(HtmlCompat.fromHtml(text, HtmlCompat.FROM_HTML_MODE_LEGACY))
+            adapter?.addItem(
+                ContextCompat.getDrawable(this, R.drawable.ic_profile_48dp)!!,
+                GlobalClass.authors[i],
+                GlobalClass.comments[i]
+            )
         }
 
-        arrayAdapter?.notifyDataSetChanged() // notify arrayAdapter that data has changed
-        listCommentsView?.adapter = arrayAdapter
+        adapter?.notifyDataSetChanged() // notify arrayAdapter that data has changed
+        listCommentsView?.adapter = adapter
 //        textView.text = HtmlCompat.fromHtml(text, HtmlCompat.FROM_HTML_MODE_LEGACY)
     }
 }
