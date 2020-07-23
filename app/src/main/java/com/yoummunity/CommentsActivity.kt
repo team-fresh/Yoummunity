ef6b99a1c85278b33eb79c9301408c17117a6881
Soeun <iamsoeunlee@daum.net>
2020-07-23 15:22:48 +0900

    :gift_heart: activity_comments.xml: scroll view -> list view

--- app/src/main/java/com/yoummunity/CommentsActivity.kt
+++ app/src/main/java/com/yoummunity/CommentsActivity.kt
@@ -1,18 +1,32 @@
 package com.yoummunity
 
 import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.text.Html
 import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.HtmlCompat
 import kotlinx.android.synthetic.main.activity_comments.*
+import kotlinx.android.synthetic.main.list_item_comment.*
 
 class CommentsActivity : Activity() {
-    var text: String = ""
+    private var text: String = ""
+    private var arrayList = mutableListOf<String>()
+    private var arrayAdapter: ArrayAdapter<String>? = null
+    private var listComments: ListView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_comments)
+
+        listComments = list_comments
+        arrayAdapter = ArrayAdapter<String>(this, R.layout.list_item_comment, arrayList)
 
         window.attributes.gravity =
             Gravity.BOTTOM + Gravity.RIGHT  // set position of dialog on window
@@ -23,16 +37,17 @@ class CommentsActivity : Activity() {
 
     override fun onResume() {
         super.onResume()
-        var textView = textview_comment
+//        var textView = textview_comment
 //        var data = intent.getStringExtra("data")
-        if (GlobalClass.authors.isEmpty() or GlobalClass.comments.isEmpty()) {
-            textView.text = getString(R.string.guide_no_comment)
-            return
-        }
         for (i in GlobalClass.comments.indices) {
-            text += "<b>" + GlobalClass.authors[i] + "</b><br>" + GlobalClass.comments[i] + "<br><br>"
+            text =
+                "<b>" + GlobalClass.authors[i] + "</b><br>" + GlobalClass.comments[i] + "<br><br>"
             // text: String in HTML
+            arrayList.add(text)
         }
-        textView.text = HtmlCompat.fromHtml(text, HtmlCompat.FROM_HTML_MODE_LEGACY)
+
+        arrayAdapter?.notifyDataSetChanged() // notify arrayAdapter that data has changed
+        listComments?.adapter = arrayAdapter
+//        textView.text = HtmlCompat.fromHtml(text, HtmlCompat.FROM_HTML_MODE_LEGACY)
     }
 }
